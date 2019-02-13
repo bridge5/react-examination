5742919db321d1bc3e2dc0476c21f282a4fd7691
Boyang <boyoung1991@foxmail.com>
2019-02-13 18:10:04 +0800

    add pagination support to the list when there are more than 5 entries

--- src/containers/PlayerListApp.js
+++ src/containers/PlayerListApp.js
@@ -5,7 +5,30 @@ import { connect } from 'react-redux';
 import { addPlayer, deletePlayer, starPlayer } from '../actions/PlayersActions';
 import { PlayerList, AddPlayerInput } from '../components';
 
+import ReactPaginate from 'react-paginate';
+
+
+// set number of players per page
+var PER_PAGE = 5;
+
 class PlayerListApp extends Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pageData: this.props.playerlist.playersById.slice(0, PER_PAGE)
+    };
+  }
+
+  handlePageClick = data => {
+    var offset = Math.ceil(data.selected * PER_PAGE);
+    var players = this.props.playerlist.playersById.slice(offset, offset + PER_PAGE);
+    this.setState({
+      pageData: players
+    });
+  };
+
   render() {
     const {
       playerlist: { playersById },
@@ -16,12 +39,29 @@ class PlayerListApp extends Component {
       deletePlayer: this.props.deletePlayer,
       starPlayer: this.props.starPlayer,
     };
+      
+    const pagination = {
+        pageCount: Math.ceil(playersById.length / PER_PAGE)
+    }
 
     return (
       <div className={styles.playerListApp}>
         <h1>NBA Players</h1>
         <AddPlayerInput addPlayer={actions.addPlayer} />
-        <PlayerList players={playersById} actions={actions} />
+        <PlayerList players={this.state.pageData} actions={actions} />
+        <ReactPaginate
+          previousLabel={'previous'}
+          nextLabel={'next'}
+          breakLabel={'...'}
+          breakClassName={'break-me'}
+          pageCount={pagination.pageCount}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={this.handlePageClick}
+          containerClassName={'pagination'}
+          subContainerClassName={'pages pagination'}
+          activeClassName={'active'}
+        />
       </div>
     );
   }
