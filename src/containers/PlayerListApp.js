f2a4798baeba97243314042110e7b7bbc0feeb42
LCW <953950350@qq.com>
2020-03-02 15:15:06 +0800

    add filter params

--- src/containers/PlayerListApp.js
+++ src/containers/PlayerListApp.js
@@ -5,6 +5,7 @@ import { connect } from "react-redux";
 import { addPlayer, deletePlayer, starPlayer } from "../actions/PlayersActions";
 import { PlayerList, AddPlayerInput } from "../components";
 import ReactPaginate from "react-paginate";
+import classnames from "classnames";
 
 const PAGE_SIZE = 5;
 function PlayerListApp(props) {
@@ -20,10 +21,17 @@ function PlayerListApp(props) {
   const [listGroup, changeListGroup] = useState([]);
   const [totalPage, changeTotalPage] = useState(0);
   const [noData, changeNoData] = useState(false);
+  const [filterParams, changeFilterParams] = useState({
+    position: 'none'
+  });
 
   useEffect(() => {
-    const totalPage = Math.ceil(playersById.length / PAGE_SIZE);
-    const listGroup = playersById.reduce(
+    let listData = playersById;
+    if (filterParams.position !== 'none') listData = listData.filter(i => {
+      return i.position === filterParams.position
+    });
+    const totalPage = Math.ceil(listData.length / PAGE_SIZE);
+    const listGroup = listData.reduce(
       (pre, current, currentIndex) => {
         const currentGroup = (currentIndex / PAGE_SIZE) | 0;
         pre[currentGroup].push(current);
@@ -35,7 +43,7 @@ function PlayerListApp(props) {
     );
     changeListGroup(listGroup);
     changeTotalPage(totalPage);
-  }, [playersById]);
+  }, [playersById, filterParams]);
 
   useEffect(() => {
     if (!listGroup[activePage] && activePage !== 0) {
@@ -50,24 +58,42 @@ function PlayerListApp(props) {
   return (
     <div className={styles.playerListApp}>
       <h1>NBA Players</h1>
+      <div className="filterBox">
+        <span>filter: </span>
+        <select
+          name="position"
+          value={filterParams.position}
+          placeholder="select filter player"
+          className={classnames("form-control")}
+          onChange={(e) => {
+            changeFilterParams({
+              position: e.target.value
+            });
+          }}
+        >
+          <option value="none"></option>
+          <option value="SF">SF</option>
+          <option value="PG">PG</option>
+        </select>
+      </div>
       <AddPlayerInput addPlayer={actions.addPlayer} />
       {noData ? (
         <div className="noData">The list have no value. You can add the player you liked.</div>
       ) : (
-        <>
-          <PlayerList players={listGroup[activePage] || []} actions={actions} />
-          <ReactPaginate
-            previousLabel={"previous"}
-            nextLabel={"next"}
-            initialPage={0}
-            onPageChange={handlePageClick}
-            containerClassName={"pagination"}
-            pageCount={totalPage}
-            activeClassName={"active"}
-            pageRangeDisplayed={10}
-          />
-        </>
-      )}
+          <>
+            <PlayerList players={listGroup[activePage] || []} actions={actions} />
+            <ReactPaginate
+              previousLabel={"previous"}
+              nextLabel={"next"}
+              initialPage={0}
+              onPageChange={handlePageClick}
+              containerClassName={"pagination"}
+              pageCount={totalPage}
+              activeClassName={"active"}
+              pageRangeDisplayed={10}
+            />
+          </>
+        )}
     </div>
   );
 }
