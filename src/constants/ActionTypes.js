5be2d4029ef67252a3a1af75f26f7d284d90f005
liuqingheng <1303696791@qq.com>
2020-03-17 11:22:50 +0800

    liuqingheng test

--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -1,3 +1,6 @@
 export const ADD_PLAYER = 'ADD_PLAYER';
 export const STAR_PLAYER = 'STAR_PLAYER';
 export const DELETE_PLAYER = 'DELETE_PLAYER';
+export const PRE_PAGE = 'PRE_PAGE';
+export const NEXT_PAGE = 'NEXT_PAGE';
+export const TOGGLE_SF_PG = 'TOGGLE_SF_PG';
