9c7d50388ea3cecf43c74d96ab504c39cd79d863
Alberty <1255981471@qq.com>
2020-04-01 18:18:51 +0800

    weijia pull request

--- src/components/AddPlayerInput.js
+++ src/components/AddPlayerInput.js
@@ -1,20 +1,26 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 import styles from './AddPlayerInput.css';
+import PlayerPositionSelect from "./PlayerPositionSelect";
 
 class AddPlayerInput extends Component {
   render() {
     return (
-      <input
-        type="text"
-        autoFocus={true}
-        className={classnames('form-control', styles.addPlayerInput)}
-        placeholder="Type the name of a player"
-        value={this.state.name}
-        onChange={this.handleChange.bind(this)}
-        onKeyDown={this.handleSubmit.bind(this)}
-      />
+      <>
+        <input
+          type="text"
+          autoFocus={true}
+          className={classnames('form-control', styles.addPlayerInput)}
+          placeholder="Type the name of a player"
+          value={this.state.name}
+          onChange={this.handleChange.bind(this)}
+          onKeyDown={this.handleSubmit.bind(this)}
+        />
+        <PlayerPositionSelect
+          value={this.state.position}
+          onChange={this.handleSelectChange}/>
+      </>
     );
   }
 
@@ -22,18 +28,24 @@ class AddPlayerInput extends Component {
     super(props, context);
     this.state = {
       name: this.props.name || '',
+      position: "SF"
     };
   }
 
+  handleSelectChange = (value) => {
+    this.setState({position: value});
+  };
+
   handleChange(e) {
-    this.setState({ name: e.target.value });
+    this.setState({name: e.target.value});
   }
 
   handleSubmit(e) {
     const name = e.target.value.trim();
     if (e.which === 13) {
-      this.props.addPlayer(name);
-      this.setState({ name: '' });
+      const {position} = this.state;
+      this.props.addPlayer(name,position);
+      this.setState({name: ''});
     }
   }
 }
