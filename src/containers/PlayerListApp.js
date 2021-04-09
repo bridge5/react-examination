8cd014bd47d4b3d36489b5df3b221fbd38ec99bb
azhengi <9opher@gmail.com>
2021-04-09 22:39:11 +0800

    feat: delete plyer item tips

--- src/containers/PlayerListApp.js
+++ src/containers/PlayerListApp.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import classnames from 'classnames';
 import styles from './PlayerListApp.css';
 import { connect } from 'react-redux';
 
@@ -6,23 +7,49 @@ import { addPlayer, deletePlayer, starPlayer } from '../actions/PlayersActions';
 import { PlayerList, AddPlayerInput } from '../components';
 
 class PlayerListApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayTip: false
+    };
+  }
+
   render() {
+    const { displayTip } = this.state;
     const {
       playerlist: { playersById },
     } = this.props;
 
+    const enhancedDeletePlayer = (id) => {
+      this.setState(() => ({displayTip: true}), () => {
+        setTimeout(() => {
+          this.setState(() => ({displayTip: false}));
+        }, 2e3);
+      });
+      this.props.deletePlayer(id);
+    }
+
     const actions = {
       addPlayer: this.props.addPlayer,
-      deletePlayer: this.props.deletePlayer,
+      deletePlayer: enhancedDeletePlayer,
       starPlayer: this.props.starPlayer,
     };
 
     return (
-      <div className={styles.playerListApp}>
-        <h1>NBA Players</h1>
-        <AddPlayerInput addPlayer={actions.addPlayer} />
-        <PlayerList players={playersById} actions={actions} />
-      </div>
+      <>
+        <div className={styles.playerListApp}>
+          <h1>NBA Players</h1>
+          <AddPlayerInput addPlayer={actions.addPlayer} />
+          <PlayerList players={playersById} actions={actions} />
+        </div>
+        {
+          displayTip ? (
+            <div className={classnames("alert", "alert-primary")} role="alert">
+              Delete complete
+            </div>
+          ) : null
+        }
+      </>
     );
   }
 }
