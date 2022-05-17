298f4d626d08a7fc3ab4d67500e7ecb528d55101
fave-xuerong <58644288+fave-xuerong@users.noreply.github.com>
2022-05-17 18:25:45 +0800

    Add files via upload

---
+++ src/redux/constants/ActionTypes.js
@@ -0,0 +1,5 @@
+export const ADD_PLAYER = Symbol('ADD_PLAYER');
+export const STAR_PLAYER = Symbol('STAR_PLAYER');
+export const DELETE_PLAYER = Symbol('DELETE_PLAYER');
+export const UPDATE_PLAYER = Symbol('UPDATE_PLAYER');
+// 如果项目体积增大难以避免重名问题，这里用Symbol包裹了一下
