091e056feaf2ba9d5af6fa3aa467c2d0896ee2e0
fave-xuerong <58644288+fave-xuerong@users.noreply.github.com>
2022-05-17 18:19:46 +0800

    Add files via upload

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -2,12 +2,39 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './PlayerList.css';
 import PlayerListItem from './PlayerListItem';
+import { Pagination } from 'antd';
 
 class PlayerList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: Math.ceil(this.props.players.length / 5),
+      actionTotal: 1,
+    }
+  }
   render() {
     return (
-      <ul className={styles.playerList}>
-        {this.props.players.map((player, index) => {
+      <>
+        <ul className={styles.playerList}>
+          {this.renderList()}
+        </ul>
+        <Pagination defaultPageSize={5} defaultCurrent={1} total={this.state.total * 5} onChange={this.handleChangeTotal} />
+      </>
+    );
+  }
+
+  handleChangeTotal = newActionTotal => {
+    this.setState({
+      ...this.state,
+      actionTotal: newActionTotal
+    })
+  }
+
+  renderList = () => {
+    let showPlayers = this.props.players.slice((this.state.actionTotal - 1) * 5, this.state.actionTotal * 5);
+    return (
+      <>
+        {showPlayers.map((player, index) => {
           return (
             <PlayerListItem
               key={index}
@@ -20,8 +47,8 @@ class PlayerList extends Component {
             />
           );
         })}
-      </ul>
-    );
+      </>
+    )
   }
 }
 
